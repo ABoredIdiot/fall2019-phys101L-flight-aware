112dc296627cd99b4fbb5de7c6c8bee502ae084e
Bert Bee-Lindgren <bert@bee-lindgren.com>
2019-10-23 21:52:35 -0400

    Initialized from Spring Boot Initializr (https://start.spring.io/)

---
+++ src/main/java/net/flyingdragon/max/fall2019physflightaware/Fall2019PhysFlightAwareApplication.java
@@ -0,0 +1,13 @@
+package net.flyingdragon.max.fall2019physflightaware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fall2019PhysFlightAwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fall2019PhysFlightAwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/flyingdragon/max/fall2019physflightaware/Fall2019PhysFlightAwareApplicationTests.java
@@ -0,0 +1,13 @@
+package net.flyingdragon.max.fall2019physflightaware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fall2019PhysFlightAwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
