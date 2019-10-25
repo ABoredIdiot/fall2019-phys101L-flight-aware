1616e4fd88c69c87ab2b5b87cb9e9bd4c0022edf
ABoredIdiot <80bertbeel@csdecatur.net>
2019-10-24 22:59:54 -0400

    Added bounding boxes to properties.ex

--- src/main/resources/application.properties.ex
+++ src/main/resources/application.properties.ex
@@ -3,3 +3,24 @@ logging.level.org.apache.http=INFO
 flightaware.username = username
 flightaware.password = password
 flightaware.base_url = https://flightxml.flightaware.com/json/FlightXML2
+
+# SFO
+#location1=37.601258  -122.413616
+#location2=37.636882 -122.291737
+#maxNum=15
+
+# Oglethorpe Small
+location1=33.869752 -84.341383
+location2=33.884966 -84.328385
+maxNum=105
+
+# Oglethorpe and a bit (NE Atlanta)
+#location1=33.819882 -84.360342
+#location2=33.904839 -84.257387
+#maxNum=105
+
+# Metro Atlanta
+#location1=33.346089 -84.790950
+#location2=33.958810 -84.110096
+#maxNum=15
+
